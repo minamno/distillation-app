751f30e7e0f5ef8dadde2984837ed3f42c63a547
minamno <mmyna391@gmail.com>
2025-02-17 15:24:51 +0200

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+// src/components/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>لوحة التحكم والتحليل</h2>
+      <p>هنا ستعرض الرسوم البيانية وتحليلات الأداء.</p>
+      {/* يمكنك إضافة مكونات الرسوم البيانية هنا */}
+    </div>
+  );
+};
+
+export default Dashboard;
